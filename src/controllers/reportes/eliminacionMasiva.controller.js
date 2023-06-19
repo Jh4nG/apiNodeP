0eab4e794dda7b5cdd1a48a4efb7f4fc604aa4d8
Jh4nG <jhonja971106@gmail.com>
2023-06-19 16:58:27 -0500

    Parcial eliminacion

--- src/controllers/reportes/eliminacionMasiva.controller.js
+++ src/controllers/reportes/eliminacionMasiva.controller.js
@@ -69,15 +69,50 @@ const updateData = async (req,res) => {
 
 const deleteData = async (req,res)=>{
     try{
-        const connection = await getConnection();
-        const { id } = req.body;
-        const result = await connection.query(`DELETE FROM ${table} WHERE id = ?`,[id]);
-        if(result.affectedRows > 0){
+        const { id, name_user, username, captcha } = req.body;
+        let { group_users,parent } = req.body;
+        group_users = atob(group_users).split(',');
+        parent = atob(parent);
+        var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secret_key_captcha + "&response=" + captcha + "&remoteip=" + req.connection.remoteAddress;
+        request(verificationUrl,async function(error,response,body) {
+            body = JSON.parse(body);
+            // Success will be true or false depending upon captcha validation.
+            if(body.success !== undefined && !body.success) {
+              return res.status(400).json({status : 400, "msg" : "Error captcha, verifique nuevamente"});
+            }
+            const connection = await getConnection();
+            const insert = await connection.query(`INSERT INTO adm_listado_activos_eliminados (depto,ciudad,despacho,suscriptor,radicado,rad23,fecha_registro,proceso,demandante,demandado)
+                                                    SELECT left(despacho,2),left(despacho,5),despacho,usuario,radicacion,codigo_23,fecha_registro,proceso,demandante,demandado FROM ${table} WHERE id_userope = ?`,[id]);
+            
+            if(insert.affectedRows > 0){
+                const dataEmail = await getDataEmail(connection, [group_users,group_users,id], username);
+                const result = await connection.query(`DELETE FROM ${table} WHERE id_userope = ?`,[id]);
+                if(result.affectedRows > 0){
+                    const { valor:correo_comercial } = await global_c.getParameter(connection,5);
+                    const { valor:web_master } = await global_c.getParameter(connection,14);
+                    if(dataEmail.status){
+                        let html = `
+                                <p style="color: #000 !important;"><b>Suscriptor:</b> ${name_user} </p>
+                                <p style="color: #000 !important;"><b>Despacho:</b> ${dataEmail.despacho} </p>
+                                <p style="color: #000 !important;"><b>Ciudad:</b> ${dataEmail.municipio} </p>
+                                <p style="color: #000 !important;"><b>Departamento:</b> ${dataEmail.departamento} </p>
+                                <p style="color: #000 !important;"><b>Radicado (9 digitos):</b> ${dataEmail.radicado} </p>
+                                <p style="color: #000 !important;"><b>Radicado (23 digitos):</b> ${dataEmail.codigo_23} </p>
+                                <p style="color: #000 !important;"><b>Tipo de Proceso:</b> ${dataEmail.proceso} </p>
+                                <p style="color: #000 !important;"><b>Demandante:</b> ${dataEmail.demandante} </p>
+                                <p style="color: #000 !important;"><b>Demandado:</b> ${dataEmail.demandado} </p>
+                            `;
+                        await global_c.sendEmail(correo_corporativo, dataEmail.emails, "Eliminación de Vigilancia Judicial", html, `${correo_comercial},${web_master}`);
+                    }
+                    connection.end();
+                    return res.status(200).json({status : 200, msg : `Procesos ${msgDeleteOk}`});
+                }
+                connection.end();
+                return res.status(400).json({status : 400, msg : `${msgDeleteErr} Procesos. ${msgTry}`});
+            }
             connection.end();
-            return res.status(200).json({status : 200, msg : `EliminacionMasiva ${msgDeleteOk}`});
-        }
-        connection.end();
-        return res.status(400).json({status : 400, msg : `${msgDeleteErr} EliminacionMasiva. ${msgTry}`});
+            return res.status(400).json({status : 400, msg : `${msgDeleteErr} Procesos. ${msgTry}`, msgExtra : 'Error en inserción activos eliminados.'});
+          });
     }catch(error){
         return res.status(500).json({ status : 500, msg : error.message });
     }
